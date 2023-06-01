54454a4fade7c7e267ca18f28dd5207fdd0bd14f
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-06-01 11:43:16 +1000

    add query and marshmallow

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from datetime import date 
+import json
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# pip install: flask, flask-sqlalchemy, sqlalchemy, psycopg2-binary
 
-# When settinput up new database create a new user for the app and give them
-# correct permissions: grant all privileges on database <database_name> to <username>;
+# When setting up new database create a new user for the app 
+# CREATE USER <user> WITH PASSWORD <password>;
+# and give them
+# correct permissions: 
+# grant all privileges on database <database_name> to <username>;
+
 # print(app.config)
 # Set database connection path giving database type and adapter, then path and user/password local host port
+# Setting database connection string - this is a universal format URI for connecting to any database and must be in this configuration.
+# Databse+adapter://<user>:<password>@<host name>:port/<database>
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello'
 
+# create an instance of SQLAlchemy and pass in Flask app instance as argument to link the two together and open connectionn to DB.
 db = SQLAlchemy(app)
 # print(db.__dict__)
 
@@ -21,7 +30,46 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
+
+# CLI COMMAND AREA
+@app.route('/cards') 
+def all_cards():
+    # select * from cards
+    # ordered by desc id
+    stmt = db.select(Card).order_by(Card.status.desc())
+    cards = db.session.scalars(stmt).all()  
+    return json.dumps(cards)
+
+
+@app.cli.command('one_card') 
+def one_card():
+    # select * from cards
+    # you can limit the stmt results using .limit(number to see)
+    # this returns a list of the number of cards you want to see
+    # This is best practice because it will stop search when first result is found, and cease searching
+    # add db.or_ for or queries
+    # stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2))
+    # 'and' is default. See above for or statements
+    stmt = db.select(Card).where(Card.status != 'Done', Card.id > 2).order_by(Card.id.desc())
+    cards = db.session.scalars(stmt).all()
+    for card in cards:
+        print(card.__dict__)
+
+# @app.cli.command('one_card') 
+# def one_card():
+#     # select * from cards
+#     # you can limit the stmt results using .limit(number to see)
+#     # this returns a list of the number of cards you want to see
+#     # This is best practice because it will stop search when first result is found, and cease searching
+#     # add db.or_ for or queries
+#     # stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2))
+#     # 'and' is default. See above for or statements
+#     stmt = db.select(Card).where(Card.status != 'Done', Card.id > 2).order_by(Card.id.desc())
+#     cards = db.session.scalars(stmt).all()
+#     for card in cards:
+#         print(card.__dict__)
 
 
 @app.cli.command('create')
@@ -33,17 +81,31 @@ def create():
 @app.cli.command('seed')
 def seed_db():
     # Create an instance of the card model in memory
-    card = Card(
-        title = 'Start the project',
-        description = 'Create an ERD',
-        date_created = date.today()
-    )
-
+    cards = [
+        Card(
+            title = 'Start the project',
+            description = 'Create an ERD',
+            status = 'Done',
+            date_created = date.today()
+        ),
+        Card(
+            title = 'ORM Queries',
+            description = 'Stage 2',
+            status = 'In progress',
+            date_created = date.today()
+        ),
+        Card(
+            title = 'Marshmallow',
+            description = 'Stage 3',
+            status = 'In progress',
+            date_created = date.today()
+        )
+    ]
     # Truncate the Card table
     db.session.query(Card).delete()
 
     # Add card to the session (transaction)
-    db.session.add(card)
+    db.session.add_all(cards)
     # commit the transaction to the database
     db.session.commit()
     print('models seeded')
