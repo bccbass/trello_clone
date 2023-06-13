928369661ea090417f5cb138f8d7b22188bc9a59
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-06-13 12:11:04 +1000

    full crud added

--- blueprints/cards_bp.py
+++ blueprints/cards_bp.py
@@ -1,7 +1,8 @@
 
 
 
-from flask import Blueprint
+from datetime import date
+from flask import Blueprint, abort, request
 from sqlalchemy.exc import IntegrityError
 from flask_jwt_extended import jwt_required
 
@@ -10,16 +11,14 @@ from models.card import Card, CardSchema
 from blueprints.auth_bp import admin_required
 
 
-cards_bp = Blueprint('cards', __name__)
+cards_bp = Blueprint('cards', __name__, url_prefix='/cards/')
 
-
-
-@cards_bp.route('/cards')
+#   *READ*
+# GET ALL CARDS
+@cards_bp.route('/')
 # verifies signature on the token
 @jwt_required()
 def all_cards():
-
-    admin_required()
     # select * from cards
     # ordered by desc id
     stmt = db.select(Card).order_by(Card.status.desc())
@@ -27,3 +26,75 @@ def all_cards():
     cards = db.session.scalars(stmt).all()  
     # Marshmallow statement to convert to python data type. Creates Card schema call dump on schema instance with passed in cards obj
     return CardSchema(many=True).dump(cards)
+
+# GET ONE CARD
+@cards_bp.route("/<int:card_id>")
+@jwt_required()
+def get_card(card_id):
+    admin_required()
+    stmt = db.select(Card).filter_by(id=card_id)
+    card = db.session.scalar(stmt)
+    if card:
+        return CardSchema().dump(card)
+    else:
+        return {'error': 'resource not found'}, 404
+    
+# CREATE NEW CARD
+@cards_bp.route("/", methods=["POST"])
+@jwt_required()
+def create_card():
+    #  Load incoming post data via the Schema 
+    card_info = CardSchema().load(request.json)
+    #  Create a new card instance from card_info
+    card = Card(
+        title = card_info['title'],
+        description = card_info['description'],
+        status = card_info['status'],
+        date_created = date.today()
+    )
+    # Add and commit the new card to the session
+    db.session.add(card)
+    db.session.commit()
+    card_info = CardSchema().load(request.json)
+    if card:
+        title = card_info['title'],
+        description = card_info['description'],
+        status = card_info['status'],
+        return CardSchema().dump(card)
+    else:
+        return {'error': 'card not found'}, 404
+
+
+#  UPDATE A CARD
+@cards_bp.route('/<int:card_id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_card(card_id):
+    admin_required()
+    stmt = db.select(Card).filter_by(id=card_id)
+    card = db.session.scalar(stmt)
+    db.session.add(card)
+    db.session.commit()
+    card_info = CardSchema().load(request.json)
+    if card:
+        # Use get method to locate key value or provide default if not found
+        card.title = card_info.get('title', card.title)
+        card.description = card_info.get('description', card.description)
+        card.status = card_info.get('status', card.status)
+        db.session.commit()
+        return CardSchema().dump(card)
+    else:
+        return {'error': 'card not found'}, 404
+    
+# DELETE A CARD
+@cards_bp.route("/<int:card_id>", methods=["DELETE"])
+@jwt_required()
+def delete_card(card_id):
+    admin_required()
+    stmt = db.select(Card).filter_by(id=card_id)
+    card = db.session.scalar(stmt)
+    if card:
+        db.session.delete(card)
+        db.session.commit()
+        return {}, 200
+    else:
+        return {'error': 'resource not found'}, 404
